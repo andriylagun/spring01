70b29cd8073a48d409e223ea7ee4bc3c656a7eb2
Andriy <andriy.lagun@gmail.com>
2020-10-21 09:39:53 +0300

    Used constructor in UserController instead of setter

--- src/main/java/com/online/cinema/config/WebConfig.java
+++ src/main/java/com/online/cinema/config/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages =
-        "com.online.cinema.controller")
+@ComponentScan(basePackages = "com.online.cinema.controller")
 public class WebConfig {
 }
